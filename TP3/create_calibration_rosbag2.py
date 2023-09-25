966d38c2b114a8d664bccd59ed70af4ec8035d3e
Nicolas Soncini <soncininicolas@gmail.com>
2023-09-25 18:39:04 -0300

    Fix and rename image writing function. Add opencv dependency for future image manipulation.

--- TP3/create_calibration_rosbag2.py
+++ TP3/create_calibration_rosbag2.py
@@ -6,20 +6,54 @@ from pathlib import Path
 from rosbags.serde import serialize_cdr
 from rosbags.typesys.types import builtin_interfaces__msg__Time as Time
 from rosbags.typesys.types import sensor_msgs__msg__CompressedImage as CompressedImage
+from rosbags.typesys.types import sensor_msgs__msg__Image as Image
 from rosbags.typesys.types import std_msgs__msg__Header as Header
 import argparse
 import csv
+import cv2
 import numpy as np
 import rosbags.rosbag2 as r2
 
 
-def write_images(bagwriter, image_folder, csv_path, topic='/camera', frameid='map'):
+def write_images(bagwriter, image_folder, csv_path, topic='/camera', frameid='camera'):
 
     timestamped_images = list()
     with open(csv_path, 'r', newline='') as csvfile:
         csvreader = csv.DictReader(csvfile)
         for row in csvreader:
-            timestamped_images.append((image_folder / row['filename'], np.int64(row['#timestamp [ns]'])))
+            timestamped_images.append((image_folder / row['filename'], int(row['#timestamp [ns]'])))
+
+    conn = bagwriter.add_connection(topic, Image.__msgtype__)
+    for path, timestamp in timestamped_images:
+        message = Image(
+                Header(
+                    stamp=Time(
+                        sec=int(timestamp // 10**9),
+                        nanosec=int(timestamp % 10**9),
+                    ),
+                    frame_id=frameid,
+                ),
+                width=752,
+                height=480,
+                encoding='rgb8',
+                is_bigendian=0,
+                step=3 * 752,  # FIXME
+                data=np.fromfile(path, dtype=np.uint8), # FIXME Doesn't work
+            )
+        bagwriter.write(
+            conn,
+            timestamp,
+            serialize_cdr(message, message.__msgtype__),
+        )
+
+
+def write_compressed_images(bagwriter, image_folder, csv_path, topic='/camera', frameid='camera'):
+
+    timestamped_images = list()
+    with open(csv_path, 'r', newline='') as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        for row in csvreader:
+            timestamped_images.append((image_folder / row['filename'], int(row['#timestamp [ns]'])))
 
     conn = bagwriter.add_connection(topic, CompressedImage.__msgtype__)
     for path, timestamp in timestamped_images:
@@ -31,8 +65,8 @@ def write_images(bagwriter, image_folder, csv_path, topic='/camera', frameid='ma
                     ),
                     frame_id=frameid,
                 ),
-                format='png',  # could also be 'png'
-                data=np.fromfile(path, dtype=np.uint8),
+                format='png',
+                data=np.fromfile(path, dtype=np.uint8), # FIXME
             )
         bagwriter.write(
             conn,
@@ -63,8 +97,8 @@ if __name__ == '__main__':
     cam1_csv = (args.folder / 'mav0/cam1/data.csv')
 
     with r2.Writer(args.outfile) as bagwriter:
-        write_images(bagwriter, cam0_images, cam0_csv, topic='/cam0/')
+        write_compressed_images(bagwriter, cam0_images, cam0_csv, topic='/cam0/')
 
-        write_images(bagwriter, cam1_images, cam1_csv, topic='/cam1/')
+        write_compressed_images(bagwriter, cam1_images, cam1_csv, topic='/cam1/')
 
     print(f'Bag has been written to {args.outfile} successfully!')
