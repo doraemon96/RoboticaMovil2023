cf2f5c5e5da29f71a581db77590df03c62a7b3c5
Felipe Cinto <cintofelipe.1@gmail.com>
2023-10-06 12:34:05 -0300

    Add launch to rectify images.

---
+++ TP3/dev_ws/src/euroc_stereo2/launch/euroc_rectify.launch.py
@@ -0,0 +1,32 @@
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, TextSubstitution
+
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('stereo_image_proc'),
+                    'launch',
+                    'stereo_image_proc.launch.py'
+                ])
+            ])
+        ),
+        Node(
+            package='euroc_stereo2',
+            executable='sync',
+            remappings=[
+                ('left_sync/image', '/left/image_raw'),
+                ('left_sync/camera_info', '/left/camera_infom1'),
+                ('right_sync/image', '/right/image_raw'),
+                ('right_sync/camera_info', '/right/camera_info'),
+            ],
+        )
+    ])
