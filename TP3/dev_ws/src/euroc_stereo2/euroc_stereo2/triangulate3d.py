96bacad096fd4b9515c1dd9ca00f5fa81b47bb06
Nicolas Soncini <soncininicolas@gmail.com>
2023-10-08 18:50:08 -0300

    Change back to OpenCV's SFM Module.

--- TP3/dev_ws/src/euroc_stereo2/euroc_stereo2/triangulate3d.py
+++ TP3/dev_ws/src/euroc_stereo2/euroc_stereo2/triangulate3d.py
@@ -23,22 +23,22 @@ class TriangulatedPointCloud3D(Node):
         pts1 = np.array(match_msg.pts1).reshape((-1,2)).T
         pts2 = np.array(match_msg.pts2).reshape((-1,2)).T
         # get projection matrices
-        projmat1 = np.array(left_info.p).reshape((4,3)).T
-        projmat2 = np.array(right_info.p).reshape((4,3)).T
+        projmat1 = np.array(left_info.p).reshape((3,4))
+        projmat2 = np.array(right_info.p).reshape((3,4))
         # calculate points in homogeneous coordinates
-        points4d = cv.triangulatePoints(projmat1, projmat2, pts1, pts2)
+        points4d = cv.sfm.triangulatePoints(np.array([pts1, pts2]), np.array([projmat1, projmat2]))
         # self.get_logger().info(f'{points4d}')
-
-        def point4d_to_point32(p3d):  # FIXME?
+        
+        def point3d_to_point32(p3d):
             p32 = Point32()
-            p32.x = float(p3d[0]) / float(p3d[3])
-            p32.y = float(p3d[1]) / float(p3d[3])
-            p32.z = float(p3d[2]) / float(p3d[3])
+            p32.x = float(p3d[0])
+            p32.y = float(p3d[1])
+            p32.z = float(p3d[2])
             return p32
         
         m_pcloud = PointCloud()
         m_pcloud.header = match_msg.header
-        m_pcloud.points = [point4d_to_point32(r) for r in points4d]
+        m_pcloud.points = [point3d_to_point32(r) for r in points4d]
 
         self.p_pointcloud2.publish(m_pcloud)
 
@@ -51,7 +51,7 @@ def main(args=None):
     # Creamos subscribers a las cámaras izq y der junto a los matches
     featmatches = message_filters.Subscriber(triangulate_node, FeatureMatches, FEAT_MATCHES)
     left_info = message_filters.Subscriber(triangulate_node, CameraInfo, INFO_LEFT)
-    right_info = message_filters.Subscriber(triangulate_node, CameraInfo, INFO_LEFT)
+    right_info = message_filters.Subscriber(triangulate_node, CameraInfo, INFO_RIGHT)
 
     ts = message_filters.TimeSynchronizer([featmatches, left_info, right_info], 10)
     ts.registerCallback(triangulate_node.matches_callback)
