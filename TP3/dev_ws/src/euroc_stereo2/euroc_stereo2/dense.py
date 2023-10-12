b294d131acbc6cd35f00c2f70dff7a3276e2ae0b
Nicolas Soncini <soncininicolas@gmail.com>
2023-10-12 09:56:48 -0300

    Manually calculate matrix Q.

--- TP3/dev_ws/src/euroc_stereo2/euroc_stereo2/dense.py
+++ TP3/dev_ws/src/euroc_stereo2/euroc_stereo2/dense.py
@@ -19,8 +19,8 @@ class DenseFromDisparity(Node):
         super().__init__('dense_from_disparity')
         self.br = CvBridge()
         self.sm = cv.StereoBM.create()
-        self.p_disparity = self.create_publisher(Image, 'disparity', 10)
-        self.p_depth = self.create_publisher(Image, 'depth', 10)
+        self.p_disparity = self.create_publisher(Image, 'depth/disparity', 10)
+        self.p_depth = self.create_publisher(Image, 'depth/points', 10)
 
     def images_callback(self, left_msg, left_info, right_msg, right_info):
         # Transform ROS2 messages to OpenCV matrices
@@ -33,21 +33,34 @@ class DenseFromDisparity(Node):
         self.p_disparity.publish(disparity_msg)
 
         # # Get Q matrix
-        # left_k, left_d = left_info.k, left_info.d
-        # right_k, right_d = right_info.k, right_info.d
+        # left_k, left_d = np.array(left_info.k), np.array([left_info.d]).T
+        # right_k, right_d = np.array(right_info.k), np.array([right_info.d]).T
         # img_size = (left_info.height, left_info.width)
         # # FIXME: 
         # # - R Rotation matrix from the coordinate system of the first camera to the second camera
         # # - T Translation vector from the coordinate system of the first camera to the second camera
-        # R, T = np.identity(3), left_info.p[: -1]
+        # R, T = np.eye(3), np.array([np.array(left_info.p).reshape(3,4)[:, -1]]).T
         # R1, R2, P1, P2, Q, validPixROI1, validPixROI2 = cv.stereoRectify(
         #     left_k, left_d, right_k, right_d, img_size, R, T)
 
-        # # Compute depth points
-        # # TODO: read about bitness of disparity img, it might need to be transformed
-        # depth_img = cv.reprojectImageTo3D(disparity_img, Q)  # 3-channel floating-point image
-        # depth_msg = self.br.cv2_to_imgmsg(depth_img, encoding='passthrough')
-        # self.p_depth.publish(depth_msg)
+        # https://docs.opencv.org/3.4/d9/d0c/group__calib3d.html#ga617b1685d4059c6040827800e72ad2b6
+        cx1 = left_info.p[2]
+        cx2 = right_info.p[2]
+        cy = left_info.p[6]
+        f = left_info.p[0]
+        Tx = right_info.p[3]
+        Q = np.matrix(
+            [[ 1, 0, 0, -cx1 ],
+             [ 0, 1, 0, -cy ],
+             [ 0, 0, 0, f ],
+             [ 0, 0, -1 / Tx, (cx1 - cx2) / Tx ]]
+        )
+
+        # Compute depth points
+        # TODO: read about bitness of disparity img, it might need to be transformed
+        depth_img = cv.reprojectImageTo3D(disparity_img, Q)  # 3-channel floating-point image
+        depth_msg = self.br.cv2_to_imgmsg(depth_img, encoding='passthrough')
+        self.p_depth.publish(depth_msg)
 
 
 def main(args=None):
